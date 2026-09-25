ad24eff620df1910ba2d7dbac8ebcd5428b2c0c8
ZakyRizqiHasanain <zaky.rizqi.ft24@mail.umy.ac.id>
2026-09-25 22:37:05 +0700

    class example

---
+++ app/class.kt
@@ -0,0 +1,11 @@
+class Contact(val id: Int, var email: String)
+
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+    println(contact.email)
+
+    contact.email = "jane@gmail.com"
+
+    println(contact.email)
+}
